c91da0c4724ebe3210c7c506100249f52d3795ce
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2020-12-06 17:17:49 +1300

    Updated copyright

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -12,7 +12,7 @@ func newVersionCommand() *cobra.Command {
 		Short: "Get Edge Cluster CLI version",
 		Run: func(cmd *cobra.Command, args []string) {
 			util.PrintInfo("Edge Cluster CLI\n")
-			util.PrintInfo("Copyright (C) 2019, Micro Business Ltd.\n")
+			util.PrintInfo("Copyright (C) 2020, Micro Business Ltd.\n")
 			util.PrintYAML(util.GetVersion())
 		},
 	}
