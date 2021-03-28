6637dfb002c7e0eef13e2b5780e944189c37ae42
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2021-03-29 11:24:33 +1300

    WIP

--- cmd/edgeCluster/main.go
+++ cmd/edgeCluster/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/decentralized-cloud/edge-cluster/internal/cmd"
 	"github.com/micro-business/go-core/pkg/util"
 )
 
 func main() {
+	fmt.Println("Test")
 	rootCmd := cmd.NewRootCommand()
 	util.PrintIfError(rootCmd.Execute())
 }
